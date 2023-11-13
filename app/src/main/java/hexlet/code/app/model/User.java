650692c29f7ef571c86603b11d06006bc83bfc9e
Alexey <leha_berezovsk@mail.ru>
2023-11-13 17:49:46 +0300

    second step (application.yml, ..-prod.yml, ..-dev.yml, User.java, UserRepository.java)

---
+++ app/src/main/java/hexlet/code/app/model/User.java
@@ -0,0 +1,44 @@
+package hexlet.code.app.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Table;
+import jakarta.persistence.Column;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.EqualsAndHashCode;
+import lombok.ToString;
+
+
+import java.time.LocalDate;
+
+import static jakarta.persistence.GenerationType.IDENTITY;
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class User {
+    @Id
+    @GeneratedValue(strategy = IDENTITY)
+    @EqualsAndHashCode.Include
+    private long id;
+
+
+    @ToString.Include
+    private String firstName;
+
+    @ToString.Include
+    private String lastName;
+
+    @Column(unique = true)
+    @ToString.Include
+    private String email;
+
+    private String password;
+
+    private LocalDate createdAt;
+    private LocalDate updateAt;
+
+
+}
