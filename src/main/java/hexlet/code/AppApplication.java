c6325d3d3a1e1dd22ec53c6a816109b89c8c60ac
Alexey <leha_berezovsk@mail.ru>
2023-12-24 23:35:56 +0300

    check

--- src/main/java/hexlet/code/AppApplication.java
+++ src/main/java/hexlet/code/AppApplication.java
@@ -1,5 +1,7 @@
 package hexlet.code;
 
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.info.Info;
 import net.datafaker.Faker;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -10,6 +12,13 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @EnableJpaAuditing
 @SpringBootApplication
 
+@OpenAPIDefinition(
+        info = @Info(
+                title = "Users API",
+                version = "1.0",
+                description = "Information about users"
+        )
+)
 public class AppApplication {
     public static void main(String[] args) {
         SpringApplication.run(AppApplication.class, args);
